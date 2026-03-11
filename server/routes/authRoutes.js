e664b663c960970cc2e2c00e018c6496bd2e46e7
Nikunj Variya <75500096+itznik@users.noreply.github.com>
2026-03-12 00:40:20 +0530

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { registerUser } = require('../controllers/authController');
-const { registerUser, loginUser } = require('../controllers/authController');
+const { loginUser } = require('../controllers/authController');
 
 // Map the POST request to the registerUser function
 router.post('/register', registerUser);
