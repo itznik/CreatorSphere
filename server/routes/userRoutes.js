b20a7a4b2827d16282aadc6349595940638939f9
Nikunj Variya <75500096+itznik@users.noreply.github.com>
2026-03-12 01:21:56 +0530

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { getUserProfile } = require('../controllers/userController');
-const { protect } = require('../middleware/authMiddleware'); // Import the bouncer
+const { protect } = require('../middleware/authMiddleware');
 
-// We drop the "protect" middleware right into the route definition
+// This will help us debug in the terminal
+console.log("🛠️  DEBUG: userRoutes.js is being parsed by Node");
+
 router.get('/me', protect, getUserProfile);
 
 module.exports = router;
